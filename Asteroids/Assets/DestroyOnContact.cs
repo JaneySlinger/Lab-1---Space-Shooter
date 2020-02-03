4bface5e0562cb36ed442c26aa6f1c3516bbb556
Janey Slinger <janeyslinger@gmail.com>
2020-02-03 14:02:41 +0000

    Level restarts if ship is hit

--- Asteroids/Assets/DestroyOnContact.cs
+++ Asteroids/Assets/DestroyOnContact.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOnContact : MonoBehaviour
 {
     GameController gameController;
+    GameObject playerObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,11 @@ public class DestroyOnContact : MonoBehaviour
         if(gameController == null){
             Debug.Log("Cannot find 'GameController' script.");
         }
+
+        playerObject = GameObject.FindWithTag("Player");
+        if(playerObject == null){
+            Debug.Log("Cannot find the 'Player' object");
+        }
     }
 
     // Update is called once per frame
@@ -26,6 +32,9 @@ public class DestroyOnContact : MonoBehaviour
 
     void OnTriggerEnter(Collider other){
         gameController.AddScore(10);
+        if(other.gameObject == playerObject){
+            Application.LoadLevel(Application.loadedLevel);
+        }
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
